8e08cc4b32fd452a3ac99a327a28e67b5d2e411c
fyasc0 <43219315+fyasc0@users.noreply.github.com>
2018-09-18 15:10:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,16 +24,16 @@
  * Listen to scroll to change header opacity class
  */
 function checkScroll(){
-    var startY = $('nav').height() * 2; //The point where the navbar changes in px
+    var startY = $('.nav').height() * 2; //The point where the navbar changes in px
 
     if($(window).scrollTop() > startY){
-        $('nav').addClass("scrolled");
+        $('.nav').addClass("scrolled");
     }else{
-        $('nav').removeClass("scrolled");
+        $('.nav').removeClass("scrolled");
     }
 }
 
-if($('nav').length > 0){
+if($('.nav').length > 0){
     $(window).on("scroll load resize", function(){
         checkScroll();
     });
