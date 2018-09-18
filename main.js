b3c675f33e5a1fc853e33f4737f7678333d44adb
fyasc0 <43219315+fyasc0@users.noreply.github.com>
2018-09-18 15:07:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,22 @@
     $(window).scroll(function() {
             showImages('.fade');
     });
+
+/**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll(){
+    var startY = $('nav').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('nav').addClass("scrolled");
+    }else{
+        $('nav').removeClass("scrolled");
+    }
+}
+
+if($('nav').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
