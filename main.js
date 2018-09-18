739e27ff7d7ca89601c035e9bea2bfcd60576f8a
fyasc0 <43219315+fyasc0@users.noreply.github.com>
2018-09-18 15:47:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-    window.onload = function showImages(el) {
+    
+$(window).bind("load", function() {
+    function showImages(el) {
         var windowHeight = jQuery( window ).height();
         $(el).each(function(){
             var thisPos = $(this).offset().top;
@@ -18,6 +20,7 @@
     // if the image in the window of browser when scrolling the page, show that image
     $(window).scroll(function() {
             showImages('.fade');
+    });
     });
 
 $(function() {
